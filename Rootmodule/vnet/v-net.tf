db6d40681d055e0713c89e965f5c1a31c9fa6282
vijay <vijaykumarmekala8564@gmail.com>
2024-02-21 16:23:48 +0530

    files aree added

--- Rootmodule/vnet/v-net.tf
+++ Rootmodule/vnet/v-net.tf
@@ -1,6 +1,9 @@
 resource "azurerm_virtual_network" "example-vnet" {
     name = var.vnet_details.name
-    location = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
+    location = var.azurerm_resource_group.example.location
+    resource_group_name = var.azurerm_resource_group.example.name
     address_space = var.vnet_details.address_space
+    depends_on = [
+      azurerm_resource_group.example
+    ]
 }
--- Rootmodule/vnet/vnet-inputs.tf
+++ Rootmodule/vnet/vnet-inputs.tf
@@ -2,6 +2,8 @@ variable "vnet_details" {
     type = object({
         name=string
         address_space=list(string)
+        location=string
+        resource_group_name=string
 
     })
 }
