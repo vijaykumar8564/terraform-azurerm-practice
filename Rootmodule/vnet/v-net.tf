2609303df7f3d46f62978b01e7eac7e07788c380
vijay <vijaykumarmekala8564@gmail.com>
2024-02-21 16:10:31 +0530

    files changes

--- Rootmodule/vnet/v-net.tf
+++ Rootmodule/vnet/v-net.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "example-vnet" {
     name = var.vnet_details.name
-    location = var.azurerm_resource_group.example.location
-    resource_group_name = var.azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
     address_space = var.vnet_details.address_space
 }
